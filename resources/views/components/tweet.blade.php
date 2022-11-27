122ace0c7b274085795a6362cfc3815d54a66824
Zack Webster <zifidigital@gmail.com>
2022-11-27 22:07:39 +0530

    WIP

--- resources/views/components/tweet.blade.php
+++ resources/views/components/tweet.blade.php
@@ -4,6 +4,7 @@
 		<div class="grow">
 			<x-author :user="$user"/>
 			<div>{{ $body }}</div>
+			<img class="w-full rounded-2xl mt-3 border object-cover" src="{{ asset('seeder/photos/10.jpg') }}"/>
 			@if($tweet)
 				<div class="p-3 border rounded-2xl mt-3">
 					<div class="flex gap-x-1 items-center">
@@ -13,7 +14,7 @@
 					<div>{{ $tweet->body }}</div>
 				</div>
 			@endif
-			<div class="flex justify-between pt-3 text-gray-500">
+			<div class="flex justify-between mt-2 text-gray-500">
 				<div class="flex gap-3 items-center hover:text-sky-500">
 					<x-feathericon-message-square class="h-4 w-4"/>
 					<span class="text-sm">{{ rand(2,20) }}</span>
