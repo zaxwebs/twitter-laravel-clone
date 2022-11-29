78be61dbcc018ea50c445f57eceeed7daeab1865
Zack Webster <zifidigital@gmail.com>
2022-11-29 13:30:57 +0530

    WIP

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+use App\Models\User;
 use App\Models\Tweet;
 use App\Models\Mention;
 use App\Models\Hashtag;
@@ -35,6 +36,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('/{user:username}', function (User $user) {
 });
 
 require __DIR__.'/auth.php';
