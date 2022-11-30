1515548c7674b40ff8310aac2f2f3724e16d3880
Zack Webster <zifidigital@gmail.com>
2022-11-30 17:00:13 +0530

    WIP

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,4 +1,21 @@
 <x-main-layout>
+	<div class="px-4 py-1">
+		<div class="flex items-center gap-10">
+			<a class="w-9 h-9 hover:bg-gray-200 flex items-center justify-center rounded-full -ml-2" href="#"><x-feathericon-arrow-left /></a>
+			<div>
+				<div class="font-bold flex gap-x-1 items-center">
+					<span class="text-xl">{{ $user->name }}</span>
+					@if($user->badge)
+						<x-codicon-verified-filled class="text-sky-500 h-5"/>
+					@endif
+				</div>
+				<div style="font-size: 13px;" class="text-gray-500 -mt-0.5">370.4K Tweets</div>
+			</div>
+			
+		</div>
+		
+		
+	</div>
 	<img class="object-cover bg-gray-300 aspect-[3/1]" src="{{ asset($user->cover_image) }}"/>
     <div class="p-4">
 		<div class="flex mb-3">
