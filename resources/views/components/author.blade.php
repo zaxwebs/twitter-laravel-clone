2884c8c66aa3bf19c136259356f29215cdcb63dc
Zack Webster <zifidigital@gmail.com>
2022-12-04 22:55:00 +0530

    WIP

--- resources/views/components/author.blade.php
+++ resources/views/components/author.blade.php
@@ -2,10 +2,8 @@
 	<x-user.name-mark :user="$user" />
 	<div class="flex items-center">
 		<x-user.username :user="$user" />
-		@if(auth()->check())
 		@if($followers->contains($user))
 		<div class="text-xs ml-1 bg-gray-100 text-gray-600 py-0.5 px-1 rounded">Follows You</div>
-		@endif
 		@endif
 	</div>
 </div>
